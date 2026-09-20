da33d0a1eaa332a97ef972259bb3f1db81d37825
Avinaash <avinaashavi1212@gmail.com>
2026-09-20 22:48:35 +0530

    Time: 8 ms (89.78%), Space: 49 MB (60.42%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,5 +1,33 @@
-class Solution {
-    public int[][] merge(int[][] intervals) {
-        
+class Solution 
+{
+    public int[][] merge(int[][] intervals) 
+    {
+        if(intervals==null && intervals.length<=1)    
+        {
+            return intervals;
+        }
+
+        Arrays.sort(intervals,(a,b)->Integer.compare(a[0],b[0]));
+
+        List<int [] > list=new ArrayList<>();
+        int [] c=intervals[0];
+
+        for(int i=1;i<intervals.length;i++)
+        {
+            if(intervals[i][0]<=c[1])
+            {
+                c[1]=Math.max(c[1],intervals[i][1]);
+            }
+            else
+            {
+                list.add(c);
+                c=intervals[i];
+            }
+
+            
+        }
+
+        list.add(c);
+        return list.toArray(new int [list.size()][]);
     }
 }
